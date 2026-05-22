cd6c9e6691e65e5b7eb8170541fd1c6e15a5f2af
DhanumuriLokeshPersonal <155897820+DhanumuriLokesh@users.noreply.github.com>
2026-05-22 09:33:02 +0530

    Commit-2/Lokesh : Added, ProjectCreation.json,Methods Related

--- src/main/java/com/leadwinner/api/utils/JsonUtils.java
+++ src/main/java/com/leadwinner/api/utils/JsonUtils.java
@@ -2,7 +2,9 @@ package com.leadwinner.api.utils;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,7 +14,9 @@ public class JsonUtils {
 	/*
 	 * ObjectMapper Class
 	 * JSON Node Class
-	 * List/ArrayList*/
+	 * List/ArrayList
+	 * 
+	 * */
 	public static Object[][] getData(String filePath) throws Exception {
 
         ObjectMapper mapper = new ObjectMapper();
@@ -33,6 +37,37 @@ public class JsonUtils {
 
         return data.toArray(new Object[0][]);
     }
+	
+//	public static String readProjectCreationData(String filePath) throws Exception {
+//	    ObjectMapper mapper = new ObjectMapper();
+//	    JsonNode node = mapper.readTree(new File(filePath));
+//	    return node.toString();
+//	}
+	
+	// 
+	public static Map<String, String> getHeaders(String filePath) throws Exception {
+	    ObjectMapper mapper = new ObjectMapper();
+	    JsonNode node = mapper.readTree(new File(filePath));
+	    JsonNode headersNode = node.get("headers");
+
+	    Map<String, String> headers = new HashMap<>();
+	    headersNode.fields().forEachRemaining(field -> {
+	        headers.put(field.getKey(), field.getValue().asText());
+	    });
+	    
+	    return headers;
+	}
+
+	// 
+	public static String readProjectCreationDataBody(String filePath) throws Exception {
+	    ObjectMapper mapper = new ObjectMapper();
+	    JsonNode node = mapper.readTree(new File(filePath));
+	    return node.get("body").toString();
+	}
+	
+	
+	
+	
 //	
 //	public static void main(String[] args) throws Exception {
 //		Object[][] x = getData("src/test/resources/data/loginData.json");
--- src/test/java/com/leadwinner/api/tests/Test_UserLoginAPI.java
+++ src/test/java/com/leadwinner/api/tests/Test_UserLoginAPI.java
@@ -1,35 +1,78 @@
 package com.leadwinner.api.tests;
 
 import com.leadwinner.api.base.BaseTest;
+import com.leadwinner.api.config.ConfigManager;
 import com.leadwinner.api.constants.Endpoints;
+import com.leadwinner.api.utils.JsonUtils;
+
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class Test_UserLoginAPI extends BaseTest{
 	
-	@Test
+	static String token;
+	
+	
+	@Test(priority =1)
 	void signinWithValidDetails() {
 //		String encodedUsername = Base64.getEncoder().encodeToString("superdmin".getBytes());
 //	    String encodedPassword = Base64.getEncoder().encodeToString("superadmin".getBytes());
 		
 		HashMap<String, String> body = new HashMap<String, String>();
-		body.put("username" , "superadmin");
+		body.put("username" , "7358190473");
 		body.put("password", "123456");
 		
-		given()
+		Response res =  given()
 			.contentType(ContentType.JSON)
 			.body(body)
 		.when()
 			.post(Endpoints.SignIN)
 		.then()
-			.log().all();
+		.extract().response();
 		
+		token = res.jsonPath().get("token");
+		System.out.println(token);
+	
+	}
+	
+	
+	@Test(priority =2)
+	public void createProjectDetails() throws Exception {
+		String filePath = "src/test/resources/data/ProjectCreation.json";
+
+	    // read headers and body from same json file
+	    Map<String, String> headers = JsonUtils.getHeaders(filePath);
+	    String udAsnDTO             = JsonUtils.readProjectCreationDataBody(filePath);
+		
+	    given()
+        .header("Authorization", "Bearer " + token)
+        .headers(headers)   // ← pass all headers at once
+        .multiPart("udAsnFile", "")
+        .multiPart("udAsnDTO", udAsnDTO, "application/json")
+    .when()
+        .post("/web/udasn")
+    .then()
+        .log().all()
+        .extract().response().prettyPrint();
+	}
+	
+	@Ignore
+	@Test
+	public void data() throws Exception {
+		String filePath = "src/test/resources/data/ProjectCreation.json";
+
+		String udAsnDTO = JsonUtils.readProjectCreationDataBody(filePath);
+		System.out.println("Body: " + udAsnDTO);
 	}
 
 }
